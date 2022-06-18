6f03d880700272efd220dd6351c06d69bb67b2e5
Joy Christine <joychristin2@gmail.com>
2022-06-18 12:54:34 +0300

    add forms

--- app/forms.py
+++ app/forms.py
@@ -2,8 +2,18 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
-
+from .models import Profile,Business
 class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email' ,'password1', 'password2']
+
+class UpdateUserForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio','neighbourhood']
+
+class AddBusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        fields = ['name','description','email']
