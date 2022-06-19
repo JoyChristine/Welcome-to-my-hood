79bfecd933a1d1578ebefb1377f52c6bb0d90958
Joy Christine <joychristin2@gmail.com>
2022-06-19 09:48:26 +0300

    add join & leave hood urls

--- app/urls.py
+++ app/urls.py
@@ -1,3 +1,14 @@
+# from django.contrib import admin
+# from django.urls import path,include
+# from . import views
+
+# urlpatterns = [
+#     path('', views.register,name='register'),
+#     path('login/',views.signin,name='login'),
+#     path('home/',views.index,name='index'),
+#     path('logout/',views.logoutuser,name='logout'),
+    
+# ]
 from django.contrib import admin
 from django.urls import path,include
 from . import views
@@ -8,4 +19,9 @@ urlpatterns = [
     path('home/',views.index,name='index'),
     path('logout/',views.logoutuser,name='logout'),
     path('profile/<id>',views.updateprofile,name='profile'),
+    path('newhood/',views.createNeighbourhood,name='newhood'),
+    path('neighbourhood/<neighbourhood_id>',views.viewneighbourhood,name='hood'),
+    path('join/<neighbourhood_id>',views.join_neighbourhood,name='join'),
+    path('leave/<neighbourhood_id>',views.leave_neighbourhood,name='leave')
+
 ]
